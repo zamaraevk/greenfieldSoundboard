57239546554f16c18ee7d006c07440427605e05f
logosghost <jimkim777@gmail.com>
2016-07-12 16:14:31 -0400

    edit package.json, util functions, handler functions

---
+++ server/lib/request-handler.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var util = require('./lib/util.js');
+
+exports.renderIndex = function(req, res) {
+  res.render('index');
+};
+
+exports.signupUserForm = function(req, res) {
+  res.render('signup');
+};
+
+exports.loginUserForm = function(req, res) {
+  res.render('login');
+};
+
+exports.logoutUser = function(req, res) {
+  req.session.destroy(function() {
+    res.redirect('/login');
+  });
+};
+
+exports.fetchSounds = function(req, res) {
+  // build
+};
+
+exports.loginuser = function(req, res) {
+  // build
+};
+exports.signupUser = function(req, res) {
+  // build
+};
+
+
+
+
+
+
+// determine where call to app is happening, where ajax call is happening,
+//
