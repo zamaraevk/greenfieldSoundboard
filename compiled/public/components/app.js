0faad0d54ec16432fc194d1dfe09a74c6c4e068b
Jear Jujaroen <jearxj@Jearranais-MacBook-Pro-2.local>
2016-07-13 12:38:03 -0400

    No proble, we good

--- compiled/public/components/app.js
+++ compiled/public/components/app.js
@@ -8,7 +8,36 @@
 //  ...
 //}
 
-//sample input: 
+var testData = {
+  65: "../../foley/28134__herbertboland__grendel.wav",
+  66: "../../foley/28134__herbertboland__grendel.wav",
+  67: "../../foley/28134__herbertboland__grendel.wav",
+  68: "../../foley/28134__herbertboland__grendel.wav",
+  69: "../../foley/28134__herbertboland__grendel.wav",
+  70: "../../foley/28134__herbertboland__grendel.wav",
+  71: "../../foley/28134__herbertboland__grendel.wav",
+  72: "../../foley/28134__herbertboland__grendel.wav",
+  73: "../../foley/28134__herbertboland__grendel.wav",
+  74: "../../foley/28134__herbertboland__grendel.wav",
+  75: "../../foley/28134__herbertboland__grendel.wav",
+  76: "../../foley/28134__herbertboland__grendel.wav",
+  77: "../../foley/28134__herbertboland__grendel.wav",
+  78: "../../foley/28134__herbertboland__grendel.wav",
+  79: "../../foley/28134__herbertboland__grendel.wav",
+  80: "../../foley/28134__herbertboland__grendel.wav",
+  81: "../../foley/28134__herbertboland__grendel.wav",
+  82: "../../foley/28134__herbertboland__grendel.wav",
+  83: "../../foley/28134__herbertboland__grendel.wav",
+  84: "../../foley/28134__herbertboland__grendel.wav",
+  85: "../../foley/28134__herbertboland__grendel.wav",
+  86: "../../foley/28134__herbertboland__grendel.wav",
+  87: "../../foley/28134__herbertboland__grendel.wav",
+  88: "../../foley/28134__herbertboland__grendel.wav",
+  89: "../../foley/28134__herbertboland__grendel.wav",
+  90: "../../foley/28134__herbertboland__grendel.wav"
+};
+
+//sample input:
 //This example would bind the 'a' key to the "example.wav" file.
 //{
 //  65: '/path/to/example'
@@ -17,37 +46,93 @@
 //For a comprehensive list of keycode bindings, see "keycode.js"
 //in this same directory.
 
-exports.App = function () {
+var App = React.createClass({
+  displayName: "App",
+
+  getInitialState: function getInitialState() {
+    return { viewHTML: React.createElement(
+        "div",
+        null,
+        " no data to show "
+      ) };
+  },
   //helper that given a path, modified a dom audio element with the given path
-  var setCurrentSong = function setCurrentSong(path, cb) {
+  setCurrentSong: function setCurrentSong(path, cb) {
     var audio = document.getElementById("mainAudio");
+    //ajax call that actually returns file
     audio.src = path;
     if (cb) {
       cb();
     }
-  };
+  },
 
   //returns a div with a keypress listener, which will call setCurrentSong
   //on the provided path when the target key is pressed.
-  var vKey = function vKey(targetKeyCode, path) {
-    return React.createElement("div", { onKeyPress: function onKeyPress(event) {
-        if (event.keyCode === targetKeyCode) {
-          setCurrentSong(path);
-        }
-      } });
-  };
 
-  ///returns a string of html representing all of the keybindings provided
-  //as input.
-  var init = function init(keyBindings) {
-    var result = "";
-    for (var code in keyBindings) {
-      result += vKey(code, keyBindings[code]);
+  loadFile: function loadFile() {
+    var vKey = function vKey(targetKeyCode, path) {
+      return React.createElement(
+        "div",
+        { onKeyPress: function onKeyPress(event) {
+            //add audio tag here?
+            // if (event.keyCode === targetKeyCode) {
+            //   this.setCurrentSong(path);
+            // }
+          } },
+        React.createElement("audio", { src: "" })
+      )
+      ////
+      ;
+    };
+    //DO NOT DELETE!  This has been commented out for testing purposes but will
+    //be necessary for communicating with the server.
+
+    //   $.get({
+    //   url: '/',
+    //   dataType: 'json',
+    //   error: function(err) {
+    //     console.error(err);
+    //   }.bind(this)
+    // }).done(function(data) {
+    //   var result = <div></div>;
+    //   for (var code in data) {
+    //     result.append(vKey(code, data[code]));
+    //   }
+    //   this.setState({viewHTML: result});
+    //   this.render();
+    // });
+    var data = testData;
+
+    var newData = [];
+
+    for (var code in data) {
+      newData.push(vKey(code, data[code]));
     }
-    return result;
-  };
 
-  return {
-    init: init(keyBindings)
-  };
-};
+    var result = React.createElement(
+      "div",
+      null,
+      newData.map(function (element) {
+        return element;
+      })
+    );
+    console.log(undefined);
+    undefined.setState({ viewHTML: result });
+    // this.render();
+  },
+
+  render: function render() {
+    console.log('1', this);
+    this.loadFile();
+    //returns a string of html representing all of the keybindings provided
+    //as input.
+    console.log('2', this);
+    return this.state.viewHTML;
+  }
+});
+
+ReactDOM.render(React.createElement(
+  "div",
+  null,
+  React.createElement(App, null)
+), document.getElementById('app'));
--- public/components/app.jsx
+++ public/components/app.jsx
@@ -6,7 +6,36 @@
 //  ...
 //}
 
-//sample input:  
+var testData = {
+  65 : "../../foley/28134__herbertboland__grendel.wav",
+  66 : "../../foley/28134__herbertboland__grendel.wav",
+  67 : "../../foley/28134__herbertboland__grendel.wav",
+  68 : "../../foley/28134__herbertboland__grendel.wav",
+  69 : "../../foley/28134__herbertboland__grendel.wav",
+  70 : "../../foley/28134__herbertboland__grendel.wav",
+  71 : "../../foley/28134__herbertboland__grendel.wav",
+  72 : "../../foley/28134__herbertboland__grendel.wav",
+  73 : "../../foley/28134__herbertboland__grendel.wav",
+  74 : "../../foley/28134__herbertboland__grendel.wav",
+  75 : "../../foley/28134__herbertboland__grendel.wav",
+  76 : "../../foley/28134__herbertboland__grendel.wav",
+  77 : "../../foley/28134__herbertboland__grendel.wav",
+  78 : "../../foley/28134__herbertboland__grendel.wav",
+  79 : "../../foley/28134__herbertboland__grendel.wav",
+  80 : "../../foley/28134__herbertboland__grendel.wav",
+  81 : "../../foley/28134__herbertboland__grendel.wav",
+  82 : "../../foley/28134__herbertboland__grendel.wav",
+  83 : "../../foley/28134__herbertboland__grendel.wav",
+  84 : "../../foley/28134__herbertboland__grendel.wav",
+  85 : "../../foley/28134__herbertboland__grendel.wav",
+  86 : "../../foley/28134__herbertboland__grendel.wav",
+  87 : "../../foley/28134__herbertboland__grendel.wav",
+  88 : "../../foley/28134__herbertboland__grendel.wav",
+  89 : "../../foley/28134__herbertboland__grendel.wav",
+  90 : "../../foley/28134__herbertboland__grendel.wav"
+};
+
+//sample input:
 //This example would bind the 'a' key to the "example.wav" file.
 //{
 //  65: '/path/to/example'
@@ -16,7 +45,10 @@
 //in this same directory.
 
 
-exports.App = React.createClass({
+var App = React.createClass({
+  getInitialState:  function() {
+    return{viewHTML: <div> no data to show </div>}
+  },
   //helper that given a path, modified a dom audio element with the given path
   setCurrentSong: (path, cb) => {
     var audio = document.getElementById("mainAudio");
@@ -27,36 +59,71 @@ exports.App = React.createClass({
     }
   },
 
-  //returns a div with a keypress listener, which will call setCurrentSong 
-  //on the provided path when the target key is pressed. 
-  vKey: (targetKeyCode, path) => (
-      <div onKeyPress={function(event) {
-          //add audio tag here?
-          if (event.keyCode === targetKeyCode) {
-            this.setCurrentSong(path);
+  //returns a div with a keypress listener, which will call setCurrentSong
+  //on the provided path when the target key is pressed.
+
+  loadFile: () => {
+    var vKey = (targetKeyCode, path) => (
+        <div onKeyPress={function(event) {
+            //add audio tag here?
+            // if (event.keyCode === targetKeyCode) {
+            //   this.setCurrentSong(path);
+            // }
           }
-        }
-      }></div>
-      ////
-    );
+        }>
+        <audio src=""></audio>
+        </div>
+        ////
+      );
+  //DO NOT DELETE!  This has been commented out for testing purposes but will
+  //be necessary for communicating with the server.
+
+  //   $.get({
+  //   url: '/',
+  //   dataType: 'json',
+  //   error: function(err) {
+  //     console.error(err);
+  //   }.bind(this)
+  // }).done(function(data) {
+  //   var result = <div></div>;
+  //   for (var code in data) {
+  //     result.append(vKey(code, data[code]));
+  //   }
+  //   this.setState({viewHTML: result});
+  //   this.render();
+  // });
+  var data = testData;
+
+  var newData = [];
+
+  for (var code in data) {
+    newData.push(vKey(code, data[code]));
+  }
+
+  var result = (<div>
+
+    {
+      newData.map(function(element) {
+        return element;
+      })
+    }
+    </div>);
+    console.log(this);
+    this.setState({viewHTML: result});
+  // this.render();
+},
 
   render: function() {
-
-    //returns a string of html representing all of the keybindings provided 
+    console.log('1', this);
+    this.loadFile();
+    //returns a string of html representing all of the keybindings provided
     //as input.
-    $.get({
-      url: __dirname + "/soundboard",
-      dataType: 'json',
-      error: function(err) {
-        console.error(err);
-      }.bind(this)
-    }).done(function(data) {      
-      var result = "";
-      for (var code in data) {
-        result += vKey(code, data[code]);
-      }
-      this.setState({data: result});
-    });
+    console.log('2', this);
+    return this.state.viewHTML;
   }
 })
 
+ReactDOM.render(<div>
+  <App/>
+  </div>, document.getElementById('app')
+);
