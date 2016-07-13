314c6a923b8acb47daea9e0da70c236ba1dba982
Tim Clark <TC.Clark25@gmail.com>
2016-07-13 18:22:57 -0400

    mvp maybe?

--- compiled/public/components/app.js
+++ compiled/public/components/app.js
@@ -9,10 +9,32 @@
 //}
 
 var testData = {
-  97: "soundfiles/beads.wav",
-  98: "soundfiles/beltbuckle.wav",
-  99: "soundfiles/footsteps.wav",
-  100: "soundfiles/grendel.wav"
+  97: "/soundfiles/beads.wav",
+  98: "/soundfiles/beltbuckle.wav",
+  99: "/soundfiles/footsteps.wav",
+  100: "/soundfiles/grendel.wav",
+  101: "/soundfiles/beads.wav",
+  102: "/soundfiles/beltbuckle.wav",
+  103: "/soundfiles/footsteps.wav",
+  104: "/soundfiles/grendel.wav",
+  105: "/soundfiles/beads.wav",
+  106: "/soundfiles/beltbuckle.wav",
+  107: "/soundfiles/footsteps.wav",
+  108: "/soundfiles/grendel.wav",
+  109: "/soundfiles/beads.wav",
+  110: "/soundfiles/beltbuckle.wav",
+  111: "/soundfiles/footsteps.wav",
+  112: "/soundfiles/grendel.wav",
+  113: "/soundfiles/beltbuckle.wav",
+  114: "/soundfiles/footsteps.wav",
+  115: "/soundfiles/grendel.wav",
+  116: "/soundfiles/beads.wav",
+  117: "/soundfiles/beltbuckle.wav",
+  118: "/soundfiles/footsteps.wav",
+  119: "/soundfiles/grendel.wav",
+  120: "/soundfiles/beads.wav",
+  121: "/soundfiles/beltbuckle.wav",
+  122: "/soundfiles/footsteps.wav"
 };
 
 //sample input:
