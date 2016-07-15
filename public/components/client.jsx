216693af58aa1b285baa828787c2b4acd459faec
Jearxj <jear.jujaroen@gmail.com>
2016-07-15 18:00:47 -0400

    loading page works! Nyan nyan nyan...

--- public/components/client.jsx
+++ public/components/client.jsx
@@ -3,9 +3,9 @@
 class Layout extends React.Component {
   render() {
     return (
-      <div className="keyboard">
-        <h1>Welcome to Soundboard!</h1>
-        <img src="assets/tumblr_luxr3mmGVw1r1sjguo1_400.gif">
+      <div className="loading">
+        <h1 className="intro">Soundboard Loading...</h1>
+        <img src="styles/tumblr_luxr3mmGVw1r1sjguo1_400.gif"/>
       </div>
     );
   }
