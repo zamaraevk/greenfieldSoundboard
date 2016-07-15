fde41b7981fc5e542d87bf669f6d878df1cae808
Jearxj <jear.jujaroen@gmail.com>
2016-07-15 15:24:38 -0400

    changes made to dist

--- dist/bundle.js
+++ dist/bundle.js
@@ -84,6 +84,8 @@
 	  122: "/soundfiles/footsteps.wav"
 	};
 	
+	var qwertyMap = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 97, 115, 100, 102, 103, 104, 106, 107, 108, 0, 122, 120, 99, 118, 98, 110, 109];
+	
 	//sample input:
 	//This example would bind the 'a' key to the "example.wav" file.
 	//{
@@ -95,21 +97,36 @@
 	var VKey = React.createClass({
 	  displayName: "VKey",
 	
+	  // the initial state houses the player, which is set to false.
 	  getInitialState: function getInitialState() {
-	    return { playing: false };
+	    return {
+	      playing: false
+	    };
 	  },
-	
+	  // when a key is pressed, change key color, set player to true, and play it.
 	  handleKeyPress: function handleKeyPress(event) {
 	    if ("" + event.keyCode === "" + this.props.targetKey) {
+	      $('#' + event.keyCode).parent().removeClass('key');
+	      $('#' + event.keyCode).parent().addClass('blue');
 	      this.setState({ playing: true });
 	      document.getElementById(this.props.targetKey).play();
 	      event.preventDefault();
 	    }
 	    this.render();
 	  },
+	
+	  handleAudioEnd: function handleAudioEnd(event) {
+	    $('#' + this.props.targetKey).parent().removeClass('blue');
+	    $('#' + this.props.targetKey).parent().addClass('key');
+	    event.preventDefault();
+	    this.render();
+	  },
+	
 	  componentDidMount: function componentDidMount(event) {
 	    window.addEventListener('keypress', this.handleKeyPress);
+	    // window.addEventListener('ended', this.handleKeyUp);
 	  },
+	
 	  render: function render() {
 	    return React.createElement(
 	      "div",
@@ -119,28 +136,30 @@
 	        { className: "keyLabel" },
 	        keyCodes[this.props.targetKey]
 	      ),
-	      React.createElement("audio", { id: this.props.targetKey, src: this.props.path })
+	      React.createElement("audio", { id: this.props.targetKey, src: this.props.path, onEnded: this.handleAudioEnd })
 	    );
 	  }
 	});
-	
 	var App = React.createClass({
 	  displayName: "App",
 	
-	
 	  render: function render() {
-	    var data = [];
-	
-	    for (var code in testData) {
-	      data.push({ key: code,
-	        path: testData[code]
-	      });
-	    }
+	    qwertyMap = qwertyMap.map(function (key) {
+	      if (key !== 0) {
+	        return { key: key, path: testData[key] };
+	      } else {
+	        return 0;
+	      }
+	    });
 	    return React.createElement(
 	      "div",
 	      { className: "keyboard" },
-	      data.map(function (keyBinding) {
-	        return React.createElement(VKey, { targetKey: keyBinding.key, path: keyBinding.path });
+	      qwertyMap.map(function (keyBinding, idx) {
+	        if (keyBinding === 0) {
+	          return React.createElement("br", null);
+	        } else {
+	          return React.createElement(VKey, { targetKey: keyBinding.key, path: keyBinding.path });
+	        }
 	      })
 	    );
 	  }
