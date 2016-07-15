cbc3e9f3c13d03e29ac68c7ffcf4adf5440af4b6
logosghost <jimkim777@gmail.com>
2016-07-15 16:22:23 -0400

    Building Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,56 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');//linting JS files
+// var jshint = require('gulp-jshint');//linting JS files
 var changed = require('gulp-changed');//moniters files for changes to be gulped
 var webpack = require('gulp-webpack');//not sure what this does yet but it's important
 var shell = require('gulp-shell');//shell commands for pushing to heroku
-
+var webpackConfig = require('./webpack.config.js');
+var minifyHtml = require('gulp-minify-html');
+const babel = require('gulp-babel');
+const watch = require('gulp-watch');
 
 gulp.task('default', function() {
-  // place code for your default task here
+  console.log('GULP works!');
 });
 
 // JS hint task
-gulp.task('jshint', function() {
-  gulp.src('./src/scripts/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+// gulp.task('jshint', function() {
+//   gulp.src('./src/scripts/*.js') // will we create this?
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'));
+// });
+
+// gulp babel to compile components.
+gulp.task('babel', function() {
+	return gulp.src('./public/components/*')
+    // .pipe(jsx())
+		.pipe(babel({
+			presets: ['react']
+		}))
+		.pipe(gulp.dest('./compiled/public/components'));
 });
+
+// gulp watch to monitor changes in any jsx file.
+// If there is a file change, use babel task.
+gulp.task('watch', function() {
+  gulp.watch('./public/components/*.jsx', ['babel']);
+});
+
+// gulp deploy to activate Heroku commands in cli.
+gulp.task('heroku', shell.task([
+  'git push heroku master',
+  'heroku open'
+]));
+
+// gulp webpack to minify, watch, and pipe compiled js files to dist.
+gulp.task('webpack', function() {
+  return gulp.src('./compiled/components/*.js')
+  .pipe(webpack({
+    watch: true,
+    webpackConfig
+  }))
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('changed', function() {
+
+})
